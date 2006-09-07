bf8b0954f7583aa032dfab9e0046f9855edb6edb
nobody <nobody@24b68936-65cd-11e0-b8b1-f9a7680f8d76>
2006-09-07 14:03:34 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_1_0'.

---
+++ tags/RELEASE_1_1_0/KRISP/db.php
@@ -0,0 +1,86 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2006 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: JoungKyun Kim <http://www.oops.org>                          |
+// +----------------------------------------------------------------------+
+//
+// $Id: db.php,v 1.1.1.1 2006-06-20 07:49:56 oops Exp $
+
+class krdb
+{
+	var $type;
+	var $db;
+	var $err;
+	var $otype;
+
+	var $dbn = array (
+		'sqlite'  => 'sqlite2',
+		'sqlite3' => 'sqlite',
+		'mysql'   => 'mysql'
+	);
+
+	function krdb ($t) {
+		$this->type = $t;
+		$this->otype = ( $t == 'sqlite' ) ? $t : '_pdo';
+		$openfile = ($this->otype == '_pdo') ? 'pdo' : $t;
+
+		require_once $openfile . ".php";
+		$this->db = new $this->otype;
+	}
+
+	function kr_dbConnect ($database) {
+		switch ($this->type) :
+			case 'sqlite' :
+				break;
+			default :
+				$database = $this->dbn[$this->type] . ':' . $database;
+		endswitch;
+
+		$c = $this->db->sql_open ($database);
+
+		if ( $c === FALSE ) :
+			$this->err = $this->db->sql_error ();
+		endif;
+		return $c;
+	}
+
+	function kr_dbSelect ($dbh, $sql) {
+		$r = $this->db->sql_select ($dbh, $sql);
+		if ( $r === FALSE ) :
+			$this->err = $this->db->sql_error ();
+			return FALSE;
+		endif;
+
+		return $r;
+	}
+
+	function kr_dbClose ($dbh) {
+		$this->db->sql_close ($dbh);
+	}
+
+	function kr_dbError () {
+		return $this->err;
+	}
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
+?>
