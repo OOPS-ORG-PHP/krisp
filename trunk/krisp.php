eb9866eae0d47491f1d60ea79f34e3a93b0e40e4
oops <oops@24b68936-65cd-11e0-b8b1-f9a7680f8d76>
2009-10-21 16:31:33 +0000

    Add Comment of PhpDocumentor style

--- trunk/krisp.php
+++ trunk/krisp.php
@@ -1,35 +1,31 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 5                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2006 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: JoungKyun Kim <http://www.oops.org>                          |
-// +----------------------------------------------------------------------+
-//
-// $Id: krisp.php,v 1.7 2006-12-07 09:04:05 oops Exp $
-
-require_once 'PEAR.php';
+/**
+ * Project: krisp :: KRISP database frontend
+ * File:    krisp.php
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 1997-2009 JoungKyun.Kim
+ *
+ * LICENSE: GPL v2
+ *
+ * @category	Database
+ * @package		krisp
+ * @author		JoungKyun.Kim <http://oops.org>
+ * @copyright	1997-2009 OOPS.org
+ * @license		GPL v2
+ * @version		CVS: $Id: krisp.php,v 1.8 2009-10-21 16:31:33 oops Exp $
+ * @link		http://pear.oops.org/package/krisp
+ * @since		File available since release 0.0.1
+ */
 
 $_SERVER['CLI'] = $_SERVER['DOCUMENT_ROOT'] ? '' : 'yes';
 
 /**
- * PEAR's krisp:: interface. Defines the php extended krisp library
- *
- * @access public
- * @version $Revision: 1.7 $
- * @package eSystem
+ * Base class for KRISP API
+ * @package		krisp
  */
-class krisp extends PEAR
+class krisp
 {
 	var $version = "1.2.1";
 	var $uversion = "001002001";
@@ -42,6 +38,15 @@ class krisp extends PEAR
 	var $geoisp_type;
 	var $geocity_type;
 
+	// {{{ (void) krisp::krisp ($database = 'sqlite')
+	/**
+	 * Initialize KRISP class
+	 *
+	 * @access	public
+	 * @return	void
+	 * @param	string	(optional) Defaults to sqlite. Set type of krisp database.
+	 *                  Support type of database are sqlite2, sqlite, mysql
+	 */
 	function krisp ($database = 'sqlite') {
 		require_once "krisp/db.php";
 		$this->db = new krisp_db ($database);
@@ -60,16 +65,36 @@ class krisp extends PEAR
 			$this->geocity_type = GEOIP_INDEX_CACHE | GEOIP_CHECK_CACHE;
 		endif;
 	}
+	// }}}
 
+	// {{{ (string) krisp::krisp_version (void)
+	/**
+	 * Returns pear_krisp version
+	 *
+	 * @access	public
+	 * @return	string
+	 * @param	void
+	 */
 	function krisp_version () {
 		return $this->version;
 	}
+	// }}}
 
+	// {{{ (string) krisp::krisp_uversion (void)
+	/**
+	 * Returns pears version that has numeric strype
+	 *
+	 * @access	public
+	 * @return	string
+	 * @param	void
+	 */
 	function krisp_uversion () {
 		return $this->uversion;
 	}
+	// }}}
 
-	function kr_userdb ($f) {
+	// {{{ priavte (resource) krisp::kr_userdb ($f)
+	private function kr_userdb ($f) {
 		$u = '';
 
 		if ( file_exists ($f . "-userdb") ) :
@@ -83,7 +108,17 @@ class krisp extends PEAR
         
 		return $u;
 	}
+	// }}}
 
+	// {{{ (resource) krisp::kr_open ($database)
+	/**
+	 * Open the krisp database and return database handler
+	 *
+	 * @access	public
+	 * @return	resource|false If failed to open database, returns false
+	 * @param	string	Database name. If database type is set sqlite or sqlite2, set
+	 *                  sqlite database file path.
+	 */
 	function kr_open ($database) {
 		$c = $this->db->kr_dbConnect ($database);
 		if ( $c === FALSE ) :
@@ -106,7 +141,18 @@ class krisp extends PEAR
 		
 		return $r;
 	}
+	// }}}
 
+	// {{{ (array) krisp::kr_search ($dbr, $host)
+	/**
+	 * Search given hostname or ip address on krisp database and returns
+	 * information of given hostname of ip address.
+	 *
+	 * @access	public
+	 * @return	array
+	 * @param	resource	database handle by krisp::kr_open
+	 * @param	string		search host or ip address
+	 */
 	function kr_search ($dbr, $host) {
 		require_once 'krisp/krisp.php';
 		$s = new krisp_engine ($dbr);
@@ -118,7 +164,16 @@ class krisp extends PEAR
 
 		return $r;
 	}
+	// }}}
 
+	// {{{ (void) krisp::kr_close ($dbr)
+	/**
+	 * Close database handle that opend by krisp::kr_open
+	 *
+	 * @access	public
+	 * @return	void
+	 * @param	resource database handle by krisp::kr_open
+	 */
 	function kr_close ($dbr) {
 		$this->db->kr_dbClose ($dbr['handle']);
 		$this->db->kr_dbClose ($dbr['uhandle']);
@@ -132,10 +187,20 @@ class krisp extends PEAR
 			GeoIP_close ($dbr['gi']['p']);
 		endif;
 	}
+	// }}}
 
+	// {{{ (string) krisp::kr_error (void)
+	/**
+	 * Return libkrisp error string
+	 *
+	 * @access	public
+	 * @return	string	libkrisp error messages.
+	 * @parma	void
+	 */
 	function kr_error () {
 		return $this->err;
 	}
+	// }}}
 }
 
 /*
