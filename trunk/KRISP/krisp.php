396f264488a88beccc5b81badc17bc47bd9386a6
oops <oops@24b68936-65cd-11e0-b8b1-f9a7680f8d76>
2015-02-17 06:59:00 +0000

    New Year

--- trunk/KRISP/krisp.php
+++ trunk/KRISP/krisp.php
@@ -9,7 +9,7 @@
  * @package     krisp
  * @subpackage  KRISP_engine
  * @author      JoungKyun.Kim <http://oops.org>
- * @copyright   (c) 2014 JoungKyun.Kim
+ * @copyright   (c) 2015 JoungKyun.Kim
  * @license     LGPL
  * @version     $Id$
  * @link        http://pear.oops.org/package/krisp
